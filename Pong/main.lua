ac31e5282dacc9472861b9dafd801b1e7e2acff6
Mohit Maroliya <mmaroliya@gmail.com>
2019-07-30 02:19:06 +0530

    Added the movement of ball and gamestates

--- Pong/main.lua
+++ Pong/main.lua
@@ -16,6 +16,9 @@ function love.load()
     -- use nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text
     love.graphics.setDefaultFilter('nearest', 'nearest')
 
+    --use cuurent time as parameter for random function
+    math.randomseed(os.time())
+
     --font
     smallFont = love.graphics.newFont('font.ttf', 8)
     scoreFont = love.graphics.newFont('font.ttf', 32)
@@ -30,14 +33,23 @@ function love.load()
     })
 
     -- initialize score variables
-    player1Score = 0
-    player2Score = 0
+    --player1Score = 0
+    --player2Score = 0
 
     -- paddle positions on the Y axis
     player1Y = 30
     player2Y = VIRTUAL_HEIGHT - 50
 
+    -- initial ball positions
+    ballX = VIRTUAL_WIDTH / 2 - 2
+    ballY = VIRTUAL_HEIGHT / 2 - 2
 
+    --ball velocities
+    ballDX = math.random(2) == 1 and 100 or -100 --ternary operator
+    ballDY = math.random(-50, 50)
+
+    --defining state of the game
+    gameState  = 'start'
 end
 
 -- update function
@@ -45,16 +57,22 @@ function love.update(dt)
   -- body...
   --p1 scrolling
   if love.keyboard.isDown('w') then
-    player1Y =player1Y + -PADDLE_SPEED*dt  --move up
+    player1Y = math.max(0, player1Y + -PADDLE_SPEED*dt)  --move up
   elseif love.keyboard.isDown('s') then
-    player1Y = player1Y + PADDLE_SPEED*dt  --move down
+    player1Y = math.min(VIRTUAL_HEIGHT-20 ,player1Y + PADDLE_SPEED*dt)  --move down
   end
 
   --p2 scrolling
   if love.keyboard.isDown('up') then
-    player2Y =player2Y + -PADDLE_SPEED*dt --move up
+    player2Y = math.max(0, player2Y + -PADDLE_SPEED*dt) --move up
   elseif love.keyboard.isDown('down') then
-    player2Y = player2Y + PADDLE_SPEED*dt  --move down
+    player2Y = math.min(VIRTUAL_HEIGHT-20 ,player2Y + PADDLE_SPEED*dt) --move down
+  end
+
+  --update the speed of the ball
+  if gameState == 'play' then
+    ballX = ballX + ballDX * dt
+    ballY = ballY + ballDY * dt
   end
 
 end
@@ -67,20 +85,25 @@ function love.draw()
     -- to clear the scrren with a color
     --love.graphics.clear(40, 45, 52, 255)
     love.graphics.setFont(smallFont)
-    love.graphics.printf('Hello To The Pong World!', 0, 20, VIRTUAL_WIDTH, 'center')
+    if gameState == 'start' then
+        love.graphics.printf('Welcome Boi !', 0, 20, VIRTUAL_WIDTH, 'center')
+    else
+        love.graphics.printf('Now Play !', 0, 20, VIRTUAL_WIDTH, 'center')
+    end
 
     --draw score with larger FOnt
-    love.graphics.setFont(scoreFont)
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
-    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+    --love.graphics.setFont(scoreFont)
+    --love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    --love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+
     --first paddle rectangle(left side)
     love.graphics.rectangle('fill', 10, player1Y, 5, 20)
 
     --second paddle (right side)
     love.graphics.rectangle('fill', VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
-    --ball (center)    
-    love.graphics.rectangle('fill', VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
+    --ball (center)
+    love.graphics.rectangle('fill', ballX, ballY, 4, 4)
 
     -- end rendering at virtual resolution
     push:apply('end')
@@ -92,5 +115,20 @@ function love.keypressed(key)
     if key == 'backspace' then
         -- function to terminate application
         love.event.quit()
+
+        --press enter during the start state of the game, we'll go into play mode
+    elseif key == 'enter' or key == 'return' then
+        if gameState == 'start' then
+            gameState = 'play'
+        else
+            gameState = 'start'
+
+            ballX = VIRTUAL_WIDTH / 2 - 2
+            ballY = VIRTUAL_HEIGHT / 2 - 2
+
+            -- given ball's x and y velocity a random starting value
+            ballDX = math.random(2) == 1 and 100 or -100
+            ballDY = math.random(-50, 50) * 1.5
+        end
     end
 end
