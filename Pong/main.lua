ccbcb8102876379c33db3b511decddd4013d0f0a
Mohit Maroliya <mmaroliya@gmail.com>
2019-07-30 01:01:53 +0530

    Added push library

--- Pong/main.lua
+++ Pong/main.lua
@@ -1,26 +1,41 @@
 --[[
     Pong Game Development
 ]]
+push = require 'push'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
 
+--  start the Game
 function love.load()
-    love.window.setMode(WINDOW_WIDTH,WINDOW_HEIGHT, {
+    -- use nearest-neighbor filtering on upscaling and downscaling to prevent blurring of text
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    -- initialize our virtual resolution
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
         vsync = true
     })
 end
 
--- Heading of the game
-
 function love.draw()
-    love.graphics.printf(
-        'Hello Pong!',
-        0,
-        WINDOW_HEIGHT /2 -6 ,
-        WINDOW_WIDTH,
-        'center'
-    )
+    -- begin rendering at virtual resolution
+    push:apply('start')
+
+    love.graphics.printf('Hello To The Pong World!', 0, VIRTUAL_HEIGHT / 2 - 6, VIRTUAL_WIDTH, 'center')
+
+    -- end rendering at virtual resolution
+    push:apply('end')
+end
+
+-- closing function using backspace (Can use Escape also)
+function love.keypressed(key)
+    if key == 'backspace' then
+        -- function to terminate application
+        love.event.quit()
+    end
 end
