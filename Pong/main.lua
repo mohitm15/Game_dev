ae9421f4d64f9e1716f9e43fde97f66a39e99a41
Mohit Maroliya <mmaroliya@gmail.com>
2019-07-31 01:35:13 +0530

    Added the sounds on collision

--- Pong/main.lua
+++ Pong/main.lua
@@ -37,6 +37,14 @@ function love.load()
     --set this for small obejct
     love.graphics.setFont(smallFont)
 
+    --sound table
+    sounds = {
+        ['paddle_hit'] = love.audio.newSource('sounds/paddle_hit.wav', 'static'),
+        ['score'] = love.audio.newSource('sounds/score.wav', 'static'),
+        ['wall_hit'] = love.audio.newSource('sounds/wall_hit.wav', 'static')
+    }
+
+
     -- initialize our virtual resolution
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -81,6 +89,8 @@ function love.update(dt)
         else
             ball.dy = math.random(10, 150)
         end
+
+        sounds['paddle_hit']:play()
     end
 
     if ball:collides(player2) then
@@ -93,18 +103,22 @@ function love.update(dt)
         else
             ball.dy = math.random(10, 150)
         end
+
+        sounds['paddle_hit']:play()
     end
 
     -- detect upper and lower screen boundary collision and reverse if collided
     if ball.y <= 0 then
         ball.y = 0
         ball.dy = -ball.dy
+        sounds['wall_hit']:play()
     end
 
     -- -4 to account for the ball's size
     if ball.y >= VIRTUAL_HEIGHT - 4 then
         ball.y = VIRTUAL_HEIGHT - 4
         ball.dy = -ball.dy
+        sounds['wall_hit']:play()
     end
 
 
@@ -112,6 +126,7 @@ function love.update(dt)
     if ball.x < 0 then
       servingPlayer = 1
       player2Score = player2Score + 1
+      sounds['score']:play()
 
       --victory update for player2
       if player2Score == 5 then
@@ -127,6 +142,7 @@ function love.update(dt)
     if ball.x > VIRTUAL_WIDTH then
       servingPlayer = 2
       player1Score = player1Score + 1
+      sounds['score']:play()
 
       --victory updaet for player1
       if player1Score == 5 then
