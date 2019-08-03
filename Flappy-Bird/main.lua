50b54d0cc73659d307421fa04209e6e1b14f32aa
Mohit Maroliya <mmaroliya@gmail.com>
2019-08-03 06:42:24 +0530

    Implement the parralex to the screen

--- Flappy-Bird/main.lua
+++ Flappy-Bird/main.lua
@@ -11,9 +11,19 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
--- images
+-- background image and starting scroll location (X axis)
 local background = love.graphics.newImage('images/background.png')
+local backgroundScroll = 0
+-- ground image and starting scroll location (X axis)
 local ground = love.graphics.newImage('images/ground.png')
+local groundScroll = 0
+
+--speeds
+local BG_SCROLL_SPEED = 30
+local GROUND_SCROLL_SPEED =60
+
+-- point at which we should loop our background back to X 0
+local BG_LOOPING_POINT = 413
 
 function love.load()
     -- initialize our nearest-neighbor filter
@@ -34,20 +44,27 @@ function love.resize(w, h)
     push:resize(w, h)
 end
 
-function love.keypressed(key)
-    if key == 'escape' then
-        love.event.quit()
-    end
+function love.update(dt)
+  --parallex of speed looping back to 0 after the looping point
+  backgroundScroll = (backgroundScroll + BG_SCROLL_SPEED * dt) % BG_LOOPING_POINT
+  groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
 end
 
 function love.draw()
     push:start()
 
     -- draw the background
-    love.graphics.draw(background, 0, 0)
+    love.graphics.draw(background, -backgroundScroll, 0)
 
     -- draw the ground on top of the background, toward the bottom of the screen
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 12)
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
     push:finish()
+end
+
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
 end
