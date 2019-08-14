03d3b92e020c0b591e00e680b9163f3e8b427094
Mohit Maroliya <mmaroliya@gmail.com>
2019-08-14 17:42:28 +0530

    Rendered the user's message

--- Flappy-Bird/states/ScoreState.lua
+++ Flappy-Bird/states/ScoreState.lua
@@ -15,10 +15,10 @@ end
 function ScoreState:render()
     -- simply render the score to the middle of the screen
     love.graphics.setFont(flappyFont)
-    love.graphics.printf('Oof! You lost!', 0, 64, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Oof ! You lost! :(', 0, 64, VIRTUAL_WIDTH, 'center')
 
     love.graphics.setFont(mediumFont)
     love.graphics.printf('Score: ' .. tostring(self.score), 0, 100, VIRTUAL_WIDTH, 'center')
 
-    love.graphics.printf('Press Enter to Play Again!', 0, 160, VIRTUAL_WIDTH, 'center')
+    love.graphics.printf('Press Enter to play Boi ! :)', 0, 160, VIRTUAL_WIDTH, 'center')
 end
