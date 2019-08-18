5f403f08a60bea1375bb12173af4b3264216cb86
Mohit Maroliya <35539313+mohitm15@users.noreply.github.com>
2019-08-18 21:02:27 +0530

    Update main.lua

--- Breakout/main.lua
+++ Breakout/main.lua
@@ -1,27 +1,6 @@
 --[[
     GD50
     Breakout Remake
-
-    Author: Colton Ogden
-    cogden@cs50.harvard.edu
-
-    Originally developed by Atari in 1976. An effective evolution of
-    Pong, Breakout ditched the two-player mechanic in favor of a single-
-    player game where the player, still controlling a paddle, was tasked
-    with eliminating a screen full of differently placed bricks of varying
-    values by deflecting a ball back at them.
-
-    This version is built to more closely resemble the NES than
-    the original Pong machines or the Atari 2600 in terms of
-    resolution, though in widescreen (16:9) so it looks nicer on 
-    modern systems.
-
-    Credit for graphics (amazing work!):
-    https://opengameart.org/users/buch
-
-    Credit for music (great loop):
-    http://freesound.org/people/joshuaempyre/sounds/251461/
-    http://www.soundcloud.com/empyreanma
 ]]
 
 require 'src/Dependencies'
